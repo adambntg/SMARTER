325e9cd7597423226ba693004fec6ebd80078b01
NahlSyareza <availablearner@protonmail.com>
2024-12-05 19:05:19 +0700

    New man

--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -7,7 +7,10 @@
 #define MAX_UPTIME_VPIN V4
 #define WATER_VOLUME_VPIN V1
 #define TOTAL_WATER_VOLUME_VPIN V6
+#define MAX_WATER_VOLUME_VPIN V8
 #define DATE_VPIN V7
+#define WATER_VOLUME_MODE_VPIN V9
+#define OVERRIDE_MODE_VPIN V10
 
 #include <incontinens.h>
 #include <WiFi.h>
@@ -19,6 +22,7 @@ TaskHandle_t xTaskRotateMeterHandle;
 
 TimerHandle_t xUptimeTimer;
 TimerHandle_t xWaterVolumeTimer;
+TimerHandle_t xTimeoutTimer;
 
 const char *ssid = "Wokwi-GUEST";
 const char *password = "";
@@ -32,9 +36,13 @@ int filtered_rotation = 0;
 int max_rotation = 180;
 int rotation_threshold = 10;
 
-float water_volume_coeff = 0.01F;
+float water_volume_coeff = 0.12F;
 float water_volume = 0.0F;
 float total_water_volume = 0.0F;
+float max_water_volume = 0.0F;
+
+int water_volume_mode = 0;
+int override_mode = 1;
 
 void vTaskRotateMeter(void *pvParameters);
 void vUptimeTimerCallback(TimerHandle_t xTimer);
@@ -42,6 +50,27 @@ void vWaterVolumeTimerCallback(TimerHandle_t xTimer);
 void setup_rtos();
 void reset_hey_daddy();
 void yharon_synch();
+
+BLYNK_WRITE(MAX_WATER_VOLUME_VPIN)
+{
+  max_water_volume = param.asFloat();
+  Serial.print("Max water volume: ");
+  Serial.println(max_water_volume);
+}
+
+BLYNK_WRITE(WATER_VOLUME_MODE_VPIN)
+{
+  water_volume_mode = param.asInt();
+  Serial.print("Water volume mode: ");
+  Serial.println(water_volume_mode);
+}
+
+BLYNK_WRITE(OVERRIDE_MODE_VPIN)
+{
+  override_mode = param.asInt();
+  Serial.print("Override mode: ");
+  Serial.println(override_mode);
+}
 
 BLYNK_WRITE(MAX_ROTATION_VPIN)
 {
@@ -77,12 +106,19 @@ void vTaskRotateMeter(void *pvParameters)
 
   while (1)
   {
+    float wvm = water_volume_mode ? total_water_volume : water_volume;
+    if (uptime > max_uptime || wvm > max_water_volume)
+    {
+      xTimerStart(xTimeoutTimer, 0);
+    }
+    // Serial.println("LAGI SANTAI KAWAN!");
+
     int anRe = analogRead(34);
 
     rotation = anRe * 181 / 4096;
 
     filtered_rotation = rotation <= max_rotation ? rotation : max_rotation;
-    filtered_rotation *= uptime > max_uptime ? 0 : 1;
+    filtered_rotation *= xTimerIsTimerActive(xTimeoutTimer) == pdTRUE ? 0 : 1;
 
     if (rotation > rotation_threshold)
     {
@@ -95,17 +131,17 @@ void vTaskRotateMeter(void *pvParameters)
     }
     else
     {
-      if (xTimerIsTimerActive(xUptimeTimer) != pdFALSE)
+      if (xTimerIsTimerActive(xUptimeTimer) == pdTRUE)
       {
         Serial.println("Resetting timer!");
 
         uptime = 0;
-        xTimerStop(xUptimeTimer, 0);
         xTimerReset(xUptimeTimer, 0);
+        xTimerStop(xUptimeTimer, 0);
 
         water_volume = 0.0F;
-        xTimerStop(xWaterVolumeTimer, 0);
         xTimerReset(xWaterVolumeTimer, 0);
+        xTimerStop(xWaterVolumeTimer, 0);
       }
     }
 
@@ -130,9 +166,6 @@ void vUptimeTimerCallback(TimerHandle_t xTimer)
   uptime += 1;
   total_uptime += 1;
 
-  // sprintf(buff, "Uptime: %ds / Total Uptime: %ds", uptime, total_uptime);
-  // Serial.println(buff);
-
   xTimerReset(xTimer, 0);
 }
 
@@ -143,18 +176,21 @@ void vWaterVolumeTimerCallback(TimerHandle_t xTimer)
   water_volume = filtered_rotation * water_volume_coeff;
   total_water_volume += water_volume;
 
-  // sprintf(buff, "Water Volume: %.2fml", water_volume);
-  // Serial.println(buff);
-
   xTimerReset(xTimer, 0);
+}
+
+void vTimeoutTimerCallback(TimerHandle_t xTimer)
+{
+  xTimerStop(xTimer, 0);
 }
 
 void setup_rtos()
 {
-  xTaskRotateMeter = xTaskCreate(vTaskRotateMeter, "Rotate Meter Task", 8192, NULL, 1, &xTaskRotateMeterHandle);
-
   xUptimeTimer = xTimerCreate("Uptime Timer", 1000 / portTICK_PERIOD_MS, pdTRUE, (void *)0, vUptimeTimerCallback);
   xWaterVolumeTimer = xTimerCreate("Water Volume Timer", 100 / portTICK_PERIOD_MS, pdTRUE, (void *)1, vWaterVolumeTimerCallback);
+  xTimeoutTimer = xTimerCreate("Timeout Timer", 5000 / portTICK_PERIOD_MS, pdTRUE, (void *)2, vTimeoutTimerCallback);
+
+  xTaskRotateMeter = xTaskCreate(vTaskRotateMeter, "Rotate Meter Task", 8192, NULL, 1, &xTaskRotateMeterHandle);
 
   vTaskDelete(NULL);
 }
@@ -177,8 +213,12 @@ void yharon_synch()
 {
   Blynk.syncVirtual(MAX_UPTIME_VPIN);
   Blynk.syncVirtual(MAX_ROTATION_VPIN);
+  Blynk.syncVirtual(MAX_WATER_VOLUME_VPIN);
   Blynk.syncVirtual(TOTAL_UPTIME_VPIN);
   Blynk.syncVirtual(TOTAL_WATER_VOLUME_VPIN);
+  Blynk.syncVirtual(WATER_VOLUME_MODE_VPIN);
+  Blynk.syncVirtual(OVERRIDE_MODE_VPIN);
+  // Blynk.syncAll();
   Serial.println("Pins synchronized!");
 }
 
@@ -189,8 +229,8 @@ void setup()
 
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, password);
 
-  reset_hey_daddy();
-  // yharon_synch();
+  // reset_hey_daddy();
+  yharon_synch();
   setup_servo();
   setup_lcd();
   setup_rtos();
